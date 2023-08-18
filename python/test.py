e49c52c73d73dbde7caf2d7d9fdc2f431f4b5910
Gregor Riepl <gregor.riepl@swisstxt.ch>
2023-08-18 12:13:17 +0200

    Throw CodeQL a bone

--- python/test.py
+++ python/test.py
@@ -1,3 +1,4 @@
+import hashlib
 from django.forms import formsets
 
 try:
@@ -6,3 +7,13 @@ except Exception:
     pass
 
 formsets.formset_factory("vulnerable")
+
+def certificate_matches_known_hash_bad(certificate, known_hash):
+    hash = hashlib.md5(certificate).hexdigest() # BAD
+    return hash == known_hash
+
+def certificate_matches_known_hash_good(certificate, known_hash):
+    hash = hashlib.sha256(certificate).hexdigest()  # GOOD
+    return hash == known_hash
+
+certificate_matches_known_hash_bad('invalid', '0' * (hashlib.md5().digest_size * 2))
